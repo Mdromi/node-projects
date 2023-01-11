fb9e61a8a08dd1ff54e87d31efc6b3d42104bda5
Mdromi <mdromi14@gmail.com>
2023-01-12 02:40:01 +0600

    working at session and cookie

--- Realtime-pizza-order-tracker-app/server.js
+++ Realtime-pizza-order-tracker-app/server.js
@@ -1,65 +1,87 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ejs = require('ejs')
-var expressLayouts = require('express-ejs-layouts');
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+var expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const session = require("express-session");
+const flash = require("express-flash");
+// const MongoStore = require("connect-mongo")
+const MongoStore = require('connect-mongodb-session')(session);
 
-const app = express()
+const app = express();
+const MONGODB_URI = `mongodb://localhost:27017/realtime-pizza-order`;
 
 // Set Template engine
-const viewsPath = path.join(__dirname, '/resources/views')
+const viewsPath = path.join(__dirname, "/resources/views");
 
-app.set('view engine', 'ejs')
-app.use(expressLayouts) 
-app.set('views', viewsPath)
-app.use(express.static(__dirname + '/public'));
-// app.set("layout extractScripts", true)
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("views", viewsPath);
+app.use(express.static(__dirname + "/public"));
 
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-})
+require("./routes/web")(app);
 
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-})
+// Session store
+// const store = new MongoStore({
+//     mongooseConnection: mongoose.connection,
+//     uri: MONGODB_URI ,
+//     collection:'sessions'
+// })
 
-app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-})
+// //express middleware
+// app.use(session({
+//     secret: process.env.COOKIE_SECRET,
+//     resave: false,
+//     saveUnitialized: false,
+//     store: store,
+//     cookie: { maxAge: 1000 * 60 * 60 * 24 },
+// }))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+// let mongoStore = new MongoStore({
+//   mongooseConnection: mongoose.connection,
+//   collection: "sessions",
+// });
 
+// // Session config
+// app.use(
+//   session({
+//     secret: process.env.COOKIE_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     store: mongoStore,
+//     cookie: { maxAge: 1000 * 60 * 60 * 24 },
+//   })
+// );
 
+// Session store
+let mongoStore = new MongoStore({
+	mongooseConnection: mongoose.connection,
+	collection: 'sessions'
+});
+app.use(session({
+	secret: process.env.COOKIE_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	store: mongoStore,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 }
+}));
 
+app.use(flash());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-mongoose.set('strictQuery', false)
-const MONGODB_URI = `mongodb://localhost:27017/realtime-pizza-app`;
+// Database Connection
+mongoose.set("strictQuery", false);
 const PORT = process.env.PORT || 4000;
 mongoose
-    .connect(MONGODB_URI, { 
-        useNewUrlParser: true, 
-    })
-    .then(() => { 
-        console.log('MongoDB connected...');
-        app.listen(PORT, () => {
-            console.log(`Server is listing on PORT ${PORT}`);
-        })
-    })
-    .catch(e => {
-        return console.log(e);
-    }) 
+  .connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected...");
+    app.listen(PORT, () => {
+      console.log(`Server is listing on PORT ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    return console.log(e);
+  });
